ff5c154a3decb4f44f43931a1931de2447a53bda
Leticia Furlan <leticia.hrfurlan@gmail.com>
2025-06-30 22:29:00 -0300

    implementado nave sentinela

--- app/src/main/java/ifsc/poo/naves/NaveSentinela.java
+++ app/src/main/java/ifsc/poo/naves/NaveSentinela.java
@@ -1,4 +1,30 @@
 package ifsc.poo.naves;
 
-public class NaveSentinela {
+import ifsc.poo.interfaces.Autonoma;
+
+public class NaveSentinela extends NaveEspacial implements Autonoma {
+    private boolean radarAtivo;
+
+    public NaveSentinela(int velocidadeMaxima, int tripulacaoMax) {
+        super(velocidadeMaxima, tripulacaoMax);
+        this.radarAtivo = false;
+    }
+
+    public String ativarRadar() {
+        if (velocidadeAtual <= 0.9 * velocidadeMaxima) {
+            radarAtivo = true;
+            return getTagNave() + " radar ativado.";
+        }
+        return getTagNave() + " não é possível ativar o radar em alta velocidade.";
+    }
+
+    public String desativarRadar() {
+        radarAtivo = false;
+        return getTagNave() + " radar desativado.";
+    }
+
+    @Override
+    public String ativarControleAutomatico() {
+        return getTagNave() + " controle automático ativado.";
+    }
 }
